9aced9458585dfc7f3020d4556238a2d95a57c8f
PranitMalhotra <pranit.malhotra2021@vitstudent.ac.in>
2023-07-24 17:32:27 +0530

    deployment #5

--- app/config.py
+++ app/config.py
@@ -3,11 +3,7 @@ from pydantic import BaseSettings
 # Used to validate the variables found as they are env variables
 # Pydantic is also case unsensitive so does not matter the case of the object
 class Settings(BaseSettings):
-    database_hostname: str
-    database_port: str
-    database_password: str
-    database_name: str
-    database_username: str
+    database_url: str
     secret_key: str
     algorithm: str
     access_token_expire_minutes: int
